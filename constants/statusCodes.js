e0a76123f9c8e06797760e503f78866b22341b5f
jimMartin4321 <jamesrm4321@gmail.com>
2019-03-21 18:43:22 -0700

    created constants for http status codes

---
+++ constants/statusCodes.js
@@ -0,0 +1,14 @@
+module.exports = {
+  ok: 200,
+  created: 201,
+  accepted: 202,
+  permanentlyMoved: 301,
+  badRequest: 400,
+  unauthorized: 401,
+  forbidden: 403,
+  notFound: 404,
+  conflict: 409,
+  gone: 410,
+  unprocessable: 422,
+  internalServerError: 500
+};
