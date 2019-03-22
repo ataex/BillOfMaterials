97bddb9ad9b2a57ce85c731630002418dcdbe8a3
jimMartin4321 <jamesrm4321@gmail.com>
2019-03-21 18:44:51 -0700

    part creation funcionality added

--- controller/app.js
+++ controller/app.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const store = require('../model/store.js');
+const statusCodes = require('../constants/statusCodes.js');
 
 const app = express();
+const port = 3000;
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //GET requests
 
 //POST requests
+app.post('/part', (req, res) => {
+  store.createPart(req.body, (err, data) => {
+    if (err) {
+      return res.sendStatus(statusCodes.badRequest);
+    }
+    res.sendStatus(statusCodes.created);
+  });
+});
 
 //PUT requests
 
 //DELETE requests
+
+//Start Up
+app.listen(3000, (err) => {
+  if (err) {
+    return console.log('Error at server startup', err);
+  }
+  console.log('Listening on port 3000');
+});
--- model/store.js
+++ model/store.js
@@ -1,6 +1,19 @@
 const Part = require('./Part.js');
+const model = require('./model.js');
 const store = {};
 
-store.addPart = (partInfo, callback) => {
-
+store.createPart = (partInfo, callback) => {
+  const { id, name, description } = partInfo;
+  const newPart = {
+    part: new Part(id, name, description),
+    location: 'orphan',
+  };
+  if (model.nodes[id]) {
+    return callback('node already exists', null);
+  }
+  model.nodes[id] = newPart;
+  model.nodeOrg.orphans.push(id);
+  return callback(null, newPart);
 };
+
+module.exports = store;
